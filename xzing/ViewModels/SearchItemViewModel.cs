91ede24a7e4c8a35ea6c3698d32c147b4e5d26b1
Viss2019 <Viss2019@users.noreply.github.com>
2019-02-10 00:13:13 +0200

    first_commit

---
+++ xzing/ViewModels/SearchItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace xzing.ViewModels
+{
+    public class SearchItemViewModel : BaseViewModel
+    {
+        public SearchItemViewModel()
+        {
+            Title = "Search";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ xzing/Views/SearchItemPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xzing.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchItemPage : ContentPage
+    {
+        public SearchItemPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
