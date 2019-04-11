e981d108d3e229a3643cc6713a22e7fd5d0939d3
Olexandr Nazin <Olexandr.Nazin@gmail.com>
2019-04-11 04:33:48 +0300

    Create xz.js
    www

---
+++ xzing/xz.js
@@ -0,0 +1,24 @@
+<Project Sdk="Microsoft.NET.Sdk">
+
+  <PropertyGroup>
+    <TargetFramework>netstandard2.0</TargetFramework>
+    <ProduceAssemblyReference>true</ProduceAssemblyReference>
+  </PropertyGroup>
+
+  <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|AnyCPU'">
+    <DebugType>pdbonly</DebugType>
+    <DebugSymbols>true</DebugSymbols>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <PackageReference Include="Xamarin.Forms" Version="4.0.0.62955-pre2" />
+    <PackageReference Include="ZXing.Net.Mobile" Version="2.4.1" />
+    <PackageReference Include="ZXing.Net.Mobile.Forms" Version="2.4.1" />  
+  </ItemGroup>
+
+  <ItemGroup>
+    <Compile Update="Views\SearchItemPage.xaml.cs">
+      <DependentUpon>SearchItemPage.xaml</DependentUpon>
+    </Compile>
+  </ItemGroup>
+</Project>
